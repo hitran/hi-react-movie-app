9d7b13708d8e622282cf690ab2089df088c3edfb
Hi Tran <thuchitran@gmail.com>
2020-02-20 15:33:02 +0700

    add filter to Wish List

--- src/WishList/WishList.jsx
+++ src/WishList/WishList.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './WishList.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
-import Search from '../common/Search/Search';
+import { faTrash, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function WishList(props) {
-
+    const [search, setSearch] = useState('')
+    const [wishList, setWishList] = useState([]);
     const data = props.data;
     const removeFromWishList = (id) => {
         if (data.length > 0) {
@@ -13,25 +13,57 @@ export default function WishList(props) {
             props.removeFromWishList(data)
         }
     }
-    let wishList = [];
-    if (data.length > 0) {
-        wishList = data.map((movie, id) =>
-            (
-                <tr key={id}>
-                    <td>{movie.original_title}</td>
-                    <td className={styles.desktopOnly}>{movie.runtime} mins</td>
-                    <td className={styles.desktopOnly}>{movie.vote_average} / 10</td>
-                    <td><a onClick={() => removeFromWishList(id)}><FontAwesomeIcon icon={faTrash} /></a></td>
-                </tr>
-            ))
+    const onSearchChange = (e) => {
+        console.log(e.target.value);
+        const query = e.target.value;
+        setSearch(query);
+        const filteredWishList = data.filter(movie => movie.original_title.toLowerCase().includes(query.toLowerCase()));
+        let newWishList = []
+        if (filteredWishList.length > 0) {
+            newWishList = filteredWishList.map((movie, id) =>
+                (
+                    <tr key={id}>
+                        <td>{movie.original_title}</td>
+                        <td className={styles.desktopOnly}>{movie.runtime} mins</td>
+                        <td className={styles.desktopOnly}>{movie.vote_average} / 10</td>
+                        <td><a onClick={() => removeFromWishList(id)}><FontAwesomeIcon icon={faTrash} /></a></td>
+                    </tr>
+                ))
+        } else {
+            newWishList.push(<tr>
+                <td>No Result Found!</td>
+                <td className={styles.desktopOnly}></td>
+                <td className={styles.desktopOnly}></td>
+                <td></td>
+            </tr>)
+        }
+        setWishList(newWishList);
     }
+    useEffect(() => {
+        if (data.length > 0) {
+            let newWishList = data.map((movie, id) =>
+                (
+                    <tr key={id}>
+                        <td>{movie.original_title}</td>
+                        <td className={styles.desktopOnly}>{movie.runtime} mins</td>
+                        <td className={styles.desktopOnly}>{movie.vote_average} / 10</td>
+                        <td><a onClick={() => removeFromWishList(id)}><FontAwesomeIcon icon={faTrash} /></a></td>
+                    </tr>
+                ))
+            setWishList(newWishList);
+        }
+    }, [])
+    
 
     return (
         wishList.length > 0 ?
             <div className={styles.wishListWrapper}>
                 <h1 className={styles.title}>WISH LIST</h1>
                 {/* filter */}
-                <Search/>
+                <form>
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type="text" value={search} onChange={onSearchChange} placeholder="Search movie title..." />
+                </form>
                 {/* wish list*/}
                 <table>
                     <thead>
